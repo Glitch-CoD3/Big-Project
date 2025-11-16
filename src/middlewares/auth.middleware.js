7c37ede588f15c84ff41148d154e34e834b824b3
JUEL <juelrana775581@gmail.com>
2025-11-17 04:12:50 +0600

    fix bug in auth.middleware.js , problem in import asyncHandler.js , forgot to add .js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-import { asyncHandler } from "../utils/asyncHandler";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { ApiError } from "../utils/ApiError.js";
 
