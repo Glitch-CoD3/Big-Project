f10cfb825fe4fb5083dbb4ba77a1144887c0d1f0
JUEL <juelrana775581@gmail.com>
2025-11-21 14:57:10 +0600

    refactor user.routes.js: organize imports and maintain consistent formatting

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
-import { registerUser, loginUser, logOutUser, refreshAccessTokens } from "../controllers/user.controller.js";
+import {
+    registerUser, loginUser, logOutUser,
+    refreshAccessTokens, changeCurrentPassword,
+    getCurrentUser, updateAccountDetails, updateUserAvatar,
+    updateCoverImage, getUserChannelProfile,
+    getWatchHistory
+} from "../controllers/user.controller.js";
+
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,7 +31,21 @@ router.route("/login").post(
 
 
 //secured route for logout
-router.route("/logout").post( verifyJWT, logOutUser);
+router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/refresh-token").post(refreshAccessTokens);
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-details").post(verifyJWT, updateAccountDetails)
+
+//route update avatar and coverImage
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+
+//when data comes for params then it will be dynamic route
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+//watchHistory
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router;
