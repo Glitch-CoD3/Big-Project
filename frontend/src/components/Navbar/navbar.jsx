001f9632b81f1be585c124528148af7333b44b15
JUEL <juelrana775581@gmail.com>
2025-11-29 03:16:00 +0600

    setup react-vite and talwind css

---
+++ frontend/src/components/Navbar/navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='items-center text-2xl bg-gray-600 text-white w-full py-2 mt-6 text-center'>Navbar Here</div>
+  )
+}
+
+export default Navbar
